10a4efc0b0fb73507462ae0a9b94b0a9dadfda67
SnickRound <savelyreshetnik@gmail.com>
2026-03-05 00:05:12 +0300

    Add DB_URL to config.py
    Added DB_URL for database connection.

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # Твои актуальные данные
 TOKEN = "8788141370:AAFiOYGCp6ufom2u5C4Ns41FmgRDhgRfBi4"
 # Твой ID (чтобы бот тебя узнавал как хозяина)
+
 ADMIN_ID = 5167671310
+DB_URL = "postgresql://postgres:ПАРОЛЬ@db.xxxx.supabase.co:5432/postgres"
